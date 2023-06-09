c51dc5ff31f4731eb1ce89a864bc90a6a006bb6c
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2023-06-09 00:42:25 -0500

    Add Scripts

---
+++ scripts/check-version.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+printf "[KotlinMC Version Checker]\n\n"
+
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+
+kotlin_untrimmed=$(curl -s "https://api.github.com/repos/JetBrains/kotlin/releases/latest" --header "Authorization: Bearer $GITHUB_TOKEN" | grep -Po '"tag_name": "\K.*?(?=")')
+kotlin=${kotlin_untrimmed#*v}
+local_kotlin=$(cat ../versions/kotlin.txt)
+
+printf "${GREEN}Latest Kotlin Version: %s\n" "$kotlin"
+printf "${YELLOW}Local Kotlin Version: %s\n\n" "$local_kotlin"
+
+kotlin_coroutines_untrimmed=$(curl -s "https://api.github.com/repos/Kotlin/kotlinx.coroutines/releases/latest" --header "Authorization: Bearer $GITHUB_TOKEN" | grep -Po '"tag_name": "\K.*?(?=")')
+kotlin_coroutines=${kotlin_coroutines_untrimmed#*v}
+local_kotlin_coroutines=$(cat ../versions/kotlinx-coroutines.txt)
+
+printf "${GREEN}Latest Kotlin Coroutines Version: %s\n" "$kotlin_coroutines"
+printf "${YELLOW}Local Kotlin Coroutines Version: %s\n\n" "$local_kotlin_coroutines"
+
+kotlin_serialization_untrimmed=$(curl -s "https://api.github.com/repos/Kotlin/kotlinx.serialization/releases/latest" --header "Authorization: Bearer $GITHUB_TOKEN" | grep -Po '"tag_name": "\K.*?(?=")')
+kotlin_serialization=${kotlin_serialization_untrimmed#*v}
+local_kotlin_serialization=$(cat ../versions/kotlinx-serialization.txt)
+
+printf "${GREEN}Latest Kotlin Serialization Version: %s\n" "$kotlin_serialization"
+printf "${YELLOW}Local Kotlin Serialization Version: %s\n\n" "$local_kotlin_serialization"
+
+upgrade=false
+
+if [ ! "$local_kotlin" = "$kotlin" ] || [ ! "$local_kotlin_coroutines" = "$kotlin_coroutines" ] || [ ! "$local_kotlin_serialization" = "$kotlin_serialization" ]; then
+    printf "${YELLOW}Updating is Required!\n"
+    upgrade=true
+fi
+
+if [ "$upgrade" = "true" ]; then
+  bash ../scripts/upgrade-versions.sh
+else
+  printf "${GREEN}No Update Required!\n"
+fi
---
+++ scripts/upgrade-versions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+git config user.name github-actions[bot]
+git config user.email 41898282+github-actions[bot]@users.noreply.github.com
+git fetch origin master
+
+kotlin_untrimmed=$(curl -s "https://api.github.com/repos/JetBrains/kotlin/releases/latest" --header "Authorization: Bearer $GITHUB_TOKEN" | grep -Po '"tag_name": "\K.*?(?=")')
+kotlin=${kotlin_untrimmed#*v}
+
+kotlin_coroutines_untrimmed=$(curl -s "https://api.github.com/repos/Kotlin/kotlinx.coroutines/releases/latest" --header "Authorization: Bearer $GITHUB_TOKEN" | grep -Po '"tag_name": "\K.*?(?=")')
+kotlin_coroutines=${kotlin_coroutines_untrimmed#*v}
+
+kotlin_serialization_untrimmed=$(curl -s "https://api.github.com/repos/Kotlin/kotlinx.serialization/releases/latest" --header "Authorization: Bearer $GITHUB_TOKEN" | grep -Po '"tag_name": "\K.*?(?=")')
+kotlin_serialization=${kotlin_serialization_untrimmed#*v}
+
+echo "$kotlin" > ../versions/kotlin.txt
+echo "$kotlin_coroutines" > ../versions/kotlinx-coroutines.txt
+echo "$kotlin_serialization" > ../versions/kotlinx-serialization.txt
+
+if [ "$(git status --porcelain)" ]; then
+  git add ../versions/
+  git commit -m "Update Kotlin Versions"
+  git push
+  bash ./publish.sh
+else
+  echo "No changes detected"
+fi
+
