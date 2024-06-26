67926c55fa96a4167637b7e64008797adcd25a1f
Gregory Mitchell <me@gmitch215.xyz>
2024-06-25 20:59:30 -0500

    Remove Misleading Sponge Support
    Signed-off-by: Gregory Mitchell <me@gmitch215.xyz>

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,7 +60,7 @@ modrinth {
         "1.11.1", "1.11", "1.10.2", "1.10.1", "1.10", "1.9.4", "1.9.3", "1.9.2", "1.9.1", "1.9", "1.8.9", "1.8.8", "1.8.7", "1.8.6", "1.8.5", "1.8.4", "1.8.3", "1.8.2",
         "1.8.1", "1.8"
     ))
-    loaders.addAll(listOf("bukkit", "bungeecord", "folia", "paper", "purpur", "spigot", "sponge", "velocity", "waterfall"))
+    loaders.addAll(listOf("bukkit", "bungeecord", "folia", "paper", "purpur", "spigot", "velocity", "waterfall"))
     changelog.set(createChangelog())
 
     syncBodyFrom.set(rootProject.file("README.md").bufferedReader().use { it.readText() })
